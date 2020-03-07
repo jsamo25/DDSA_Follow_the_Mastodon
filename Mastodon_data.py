689fe2a5263e8f4a794203f17ea5d6e6eef0c150
Jorge <jsamo25@gmail.com>
2020-03-07 17:43:04 +0100

    Removed some methods as they require endless time to finish.
    testing "Max degree detection and ego network construction"

--- Mastodon_data.py
+++ Mastodon_data.py
@@ -1,8 +1,11 @@
 import networkx as nx
 import pandas as pd
 import simplejson as json
+import matplotlib.pyplot as plt
+from operator import itemgetter
 
 from networkx.algorithms import approximation as approx
+from networkx.algorithms import community
 
 """
 The dataset is made up by five files:
@@ -35,7 +38,6 @@ source destination crawling time of the new link. It follows the format 'YYYY-MM
 
 """                       Load the Network                  """
 
-
 mastodon_digraph = nx.read_edgelist("mastodon_first_snapshot_anonim.csv", create_using= nx.DiGraph(),delimiter=",")
 
 #To load the temporal annotated links:
@@ -58,8 +60,8 @@ The column 'CountryCode' indicates the ISO 3166-1 alpha-3 6 code of the country 
 while the column 'Location' reports the information returned by the geo-lookup service 'freegeoip.net'
 """
 instances_location = pd.read_json("instances_position.json")
-#print("Instances position:\n")
-#print(instances_location.head())
+# print("Instances position:\n")
+# print(instances_location.head())
 
 """
 data_instances_over_time.json
@@ -75,8 +77,8 @@ in a specific day is characterized by a Python dictionary storing information ab
 the number of users, connections to other instances and number of posts
 """
 instances_info = pd.read_json("data_instances_over_time.json")
-#print("Data instances over time\n")
-#print(instances_info)
+# print("Data instances over time\n")
+# print(instances_info)
 
 """
 instances_topics.json contains the topics associated to each instance. 
@@ -84,18 +86,34 @@ The file reports a JSON-object whose keys are the instances and the values are t
 To load the json file into a Python dictionary:
 """
 instance2topics = json.load(open("instances_topics.json","r"))
-#print("Instances topics\n")
-#print(instance2topics)
+# print("Instances topics\n")
+# print(instance2topics)
 
-"""                     Network Analysis Directed          """
-node_connectivity = approx.node_connectivity(mastodon_digraph)
-print("Node connectivity: ", node_connectivity)
+"""                      Histogram of degree                 """
+# Histogram = nx.degree_histogram(mastodon_digraph)
+# plt.hist(Histogram)
+# plt.grid(True)
+# plt.show()
 
-"""                     Network Analysis Undirected        """
+"""                   Max degree node + Ego Network          """
+
+# find node with largest degree
+node_and_degree = mastodon_digraph.degree()
+(largest_hub, degree) = sorted(node_and_degree, key=itemgetter(1))[-1]
+# Create ego graph of main hub
+hub_ego = nx.ego_graph(mastodon_digraph, largest_hub)
+# Draw graph
+pos = nx.spring_layout(hub_ego)
+nx.draw(hub_ego, pos, node_color='b', node_size=50, with_labels=False)
+# Draw ego as large and red
+nx.draw_networkx_nodes(hub_ego, pos, nodelist=[largest_hub], node_size=300, node_color='r')
+plt.show()
+
+"""                     Network Analysis Undirected          """
 mastodon_undirected = mastodon_digraph.to_undirected()
 
-average_clustering = approx.average_clustering(mastodon_undirected)
-print ("Average Clustering: ", average_clustering)
-
-k_components = approx.k_components(mastodon_undirected)
-print ("K Components average: %s"%str(k_components))
+# average_clustering = approx.average_clustering(mastodon_undirected)
+# print("Average Clustering: ", average_clustering)
+#
+# node_connectivity = approx.node_connectivity(mastodon_undirected)
+# print("Node connectivity: ", node_connectivity)
